692f728524cfdf80dd4ffec395a57cc02b1058fd
Nathalie Rosenkvist <rosenkvist98@gmail.com>
2025-12-12 11:36:15 +0100

    Created: header.php & added: nav & header

---
+++ app/php/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="stylesheet" href="/style.css">
+    <title>Yrgopelag</title>
+</head>
+<body>
+    <header>
+        <nav class="nav-left">
+            <ul>
+                <li><img src="" alt="logo"></li>
+            </ul>
+        </nav>
+        <nav>
+            <ul class="nav-right">
+                <li><a href="">Packadges</a></li>
+                <li><a href="">Features</a></li>
+                <li><a href="">Login</a></li>
+            </ul>
+        </nav>
+    </header>
+    <div class="hero-img">
+        <div class="stars" id="stars">stars</div>
+        <img src="" alt="hero-img">
+    </div>

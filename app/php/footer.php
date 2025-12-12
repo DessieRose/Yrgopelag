28506a824a4e861e73f5ad0bcedc014e33940fe9
Nathalie Rosenkvist <rosenkvist98@gmail.com>
2025-12-12 11:39:56 +0100

    Created: footer.php Added: a tags & script tag

---
+++ app/php/footer.php
@@ -0,0 +1,14 @@
+    <footer>
+        <ul class="footer_right">
+            <li><a href="index.php">Packadges</a></li>
+            <li><a href="destinations.php">Features</a></li>
+            <li><a href="my-trips.php">Login</a></li>
+        </ul>
+
+
+    </footer>
+
+
+    <script src="/assets/scripts/app.js"></script>
+</body>
+</html>
